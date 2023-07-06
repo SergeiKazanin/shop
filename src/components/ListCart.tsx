7c98a43851dd68f0984df7a13d3050acd606623b
SergeiKazanin <serega7667@gmail.com>
2023-07-07 06:55:25 +0700

    Edit ListCart error

--- src/components/ListCart.tsx
+++ src/components/ListCart.tsx
@@ -12,55 +12,51 @@ export default function ListCart() {
 
   return (
     <>
-      {cart.map((product) => {
-        return (
-          <div
-            key={product.id}
-            className="flex gap-3 items-center w-full p-2 rounded-lg bg-neutral-700"
-          >
-            <div className="flex items-center gap-3 w-3/5">
-              <div
-                style={{ backgroundImage: `url(${product?.images[0]})` }}
-                className="w-[50px] h-[50px] bg-center rounded-lg bg-no-repeat bg-cover"
-              ></div>
-              <Link className="hover:text-white" to={`/products/${product.id}`}>
-                {product.title}
-              </Link>
-            </div>
-            <div className="flex flex-1 justify-end gap-3 w-2/5">
-              <div className="flex items-center gap-2">
-                <button
-                  onClick={() =>
-                    addToCart({
-                      ...product,
-                      quantity: Math.max(1, product.quantity - 1),
-                    })
-                  }
-                >
-                  <RemoveIcon className="hover:text-white" />
-                </button>
-                <div className="w-10 flex justify-center">
-                  {product.quantity}
-                </div>
-                <button
-                  onClick={() =>
-                    addToCart({ ...product, quantity: product.quantity + 1 })
-                  }
-                >
-                  <AddIcon className="hover:text-white" />
-                </button>
-              </div>
-
-              <button onClick={() => delToCart(product)}>
-                <DeleteIcon className="hover:text-white" />
+      {cart.map((product) => (
+        <div
+          key={product.id}
+          className="flex gap-3 items-center w-full p-2 rounded-lg bg-neutral-700"
+        >
+          <div className="flex items-center gap-3 w-3/5">
+            <div
+              style={{ backgroundImage: `url(${product?.images[0]})` }}
+              className="w-[50px] h-[50px] bg-center rounded-lg bg-no-repeat bg-cover"
+            ></div>
+            <Link className="hover:text-white" to={`/products/${product.id}`}>
+              {product.title}
+            </Link>
+          </div>
+          <div className="flex flex-1 justify-end gap-3 w-2/5">
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() =>
+                  addToCart({
+                    ...product,
+                    quantity: Math.max(1, product.quantity - 1),
+                  })
+                }
+              >
+                <RemoveIcon className="hover:text-white" />
               </button>
-              <div className="w-28 flex justify-end whitespace-nowrap">
-                {product.price * product.quantity} $
-              </div>
+              <div className="w-10 flex justify-center">{product.quantity}</div>
+              <button
+                onClick={() =>
+                  addToCart({ ...product, quantity: product.quantity + 1 })
+                }
+              >
+                <AddIcon className="hover:text-white" />
+              </button>
+            </div>
+
+            <button onClick={() => delToCart(product)}>
+              <DeleteIcon className="hover:text-white" />
+            </button>
+            <div className="w-28 flex justify-end whitespace-nowrap">
+              {product.price * product.quantity} $
             </div>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </>
   );
 }
